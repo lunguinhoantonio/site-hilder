6b95b0a3eeadd24a0d8b784c4fcdf938f2a1473c
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-08-06 05:09:16 -0300

    Added light mode images and updated JavaScript to toggle image sources on theme switch; updated HTML to include new image classes

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ function switchTheme() {
     body.classList.toggle('light')
 
     const pfp = document.querySelector('.top-image img');
+    const meiaLuaStudios = document.querySelector('.meia-lua-studio-img');
+    const matosEntertainment = document.querySelector('.matos-entertainment-img');
+
     if(body.classList.contains('light')){
         pfp.setAttribute('src', './assets/pfp-hilder-matos-light.png');
+        meiaLuaStudios.setAttribute('src', './assets/meia-lua-studios-light.png');
+        matosEntertainment.setAttribute('src', './assets/matos-entertainment-light.png');
     } else {
         pfp.setAttribute('src', './assets/pfp-hilder-matos.png');
+        meiaLuaStudios.setAttribute('src', './assets/meia-lua-studios.png');
+        matosEntertainment.setAttribute('src', './assets/matos-entertainment.png');
     }
+
 }
 
 function backTop() {
