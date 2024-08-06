5a8bb398c7de30270411477c424fbb7da82253c3
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-08-06 04:08:05 -0300

    Refactored switchTheme and backTop functions, added util-buttons class, and modified CSS styles for buttons and layout

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 function switchTheme() {
     const body = document.querySelector('body')
     body.classList.toggle('light')
-    
+
     const pfp = document.querySelector('.top-image img');
     if(body.classList.contains('light')){
         pfp.setAttribute('src', './assets/pfp-hilder-matos-light.png');
     } else {
         pfp.setAttribute('src', './assets/pfp-hilder-matos.png');
     }
+}
+
+function backTop() {
+    button.addEventListener('click', () => {
+    document.documentElement.scrollTop = 0;
+    });
 }
