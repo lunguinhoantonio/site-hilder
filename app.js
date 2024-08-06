c346261859531bf837a0ec5bc1412742380a0507
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-08-06 02:48:01 -0300

    Added utility buttons, and dark mode toggle; updated CSS to use variables for colors and borders; refactored HTML structure and styling for various elements

---
+++ app.js
@@ -0,0 +1,11 @@
+function switchTheme() {
+    const body = document.querySelector('body')
+    body.classList.toggle('light')
+    
+    const pfp = document.querySelector('.top-image img');
+    if(body.classList.contains('light')){
+        pfp.setAttribute('src', './assets/pfp-hilder-matos-light.png');
+    } else {
+        pfp.setAttribute('src', './assets/pfp-hilder-matos.png');
+    }
+}
